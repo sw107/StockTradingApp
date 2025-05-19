4743dbb3878f5f0597e4c22e650618b0d0da96a5
sw107 <107_sw@naver.com>
2025-05-19 16:01:25 +0900

    update graph

--- stockTrading/stockTrading/ContentView.swift
+++ stockTrading/stockTrading/ContentView.swift
@@ -23,25 +23,60 @@ let fullData: [chartData] = (0..<1000).map { i in
 }
 
 
+
+
 struct ContentView: View {
     
+    struct chartData: Identifiable {
+        var id = UUID()
+        var time: Int
+        var price: Int
+    }
+    
+    
+    let fullData: [chartData] = (0..<1000).map { i in
+        chartData(
+            time: i,
+            price: Int.random(in: 100...200)
+        )
+    }
     @State private var displayData: [chartData] = []
     @State private var updatePeriod: Double = 1.0
     @State private var timer: Timer? = nil
     @State private var index: Int = 0
     @State private var indexPlus: Int = 1
     @State private var xDomain = 0...100
+    @State private var isActive = false
     
-
+    
+    @State private var chartOneSec: [chartData] = []
+    @State private var chartThreeSec: [chartData] = []
+    @State private var chartOneMin: [chartData] = []
+    
+    @State private var timerOneSec: Timer?
+    @State private var timerThreeSec: Timer?
+    @State private var timerOneMin: Timer?
+    
+    @State private var t1 = 0
+    @State private var t2 = 0
+    @State private var t3 = 0
+    
+    @State private var selectedChartIndex = 0
+    
     var body: some View {
         VStack (spacing : 20){
             HStack{ // MARK: chart
                 ScrollView(.horizontal){
-                    Chart(displayData) { item in
+                    Chart(selectChart()) { item in
                         LineMark (
                             x: .value("time", item.time),
                             y: .value("price",item.price)
                         )
+                        PointMark(
+                            x: .value("time", item.time),
+                            y: .value("price",item.price)
+                        )
+                        .symbolSize(20)
                     }
                     .chartXScale(domain: xDomain)
                     .chartYScale(domain: 50...250)
@@ -78,16 +113,14 @@ struct ContentView: View {
                 .foregroundColor(Color.gray)
             }
             .background{
-//                Color.black
+                //                Color.black
             }
         }
-        HStack(){// MARK: updatePeriod Button
+        HStack(){// MARK: change period  Button
             Button {
-                updatePeriod = 1
-                indexPlus = 1
-                xDomain = 0...100
-                restartTimer()
-            } label: {
+                selectedChartIndex = 0
+            }
+            label: {
                 Text("1초")
                     .font(.caption)
                     .bold()
@@ -102,10 +135,7 @@ struct ContentView: View {
             } // 1초 button
             
             Button {
-                updatePeriod = 3
-                indexPlus = 3
-                xDomain = 0...150
-                restartTimer()
+                selectedChartIndex = 1
             } label: {
                 Text("3초")
                     .font(.caption)
@@ -121,10 +151,7 @@ struct ContentView: View {
             } // 3초 button
             
             Button {
-                updatePeriod = 60
-                indexPlus = 60
-                xDomain = 0...500
-                restartTimer()
+                selectedChartIndex = 2
             } label: {
                 Text("1분")
                     .font(.caption)
@@ -142,296 +169,60 @@ struct ContentView: View {
         }
         .padding()
         .onAppear {
-            restartTimer()
+            startTimer()
         }
-    } // body
-    
-    func restartTimer() {
-        timer?.invalidate() // 기존 타이머 중지
-        timer = Timer.scheduledTimer(withTimeInterval: updatePeriod, repeats: true) { _ in
-            if index >= fullData.count {
-                timer?.invalidate()
-            } else {
-                displayData.append(fullData[index])
-                index += indexPlus
-            }
+        .onDisappear {
+            stopTimers()
         }
     }
-    
-} // ContentView
-
-struct threesec: View {
-
-    @Binding private var displayData: [chartData]
-    @Binding private var updatePeriod: Double
-    @Binding private var timer: Timer?
-    @Binding private var index: Int
-    @Binding private var indexPlus: Int
-    @Binding private var xDomain: ClosedRange<Double>
-    
-    var body: some View {
-        VStack (spacing : 20){
-            HStack{ // MARK: chart
-                ScrollView(.horizontal){
-                    Chart(displayData) { item in
-                        LineMark (
-                            x: .value("time", item.time),
-                            y: .value("price",item.price)
-                        )
-                    }
-                    .chartXScale(domain: xDomain)
-                    .chartYScale(domain: 50...250)
-                    .chartYAxis {
-                        AxisMarks {
-                            AxisGridLine()
-                                .foregroundStyle(Color.gray)
-                            AxisTick()
-                                .foregroundStyle(Color.gray)
-                        }
-                    }
-                    .chartXAxis {
-                        AxisMarks {
-                            AxisGridLine()
-                                .foregroundStyle(Color.gray)
-                            AxisTick()
-                                .foregroundStyle(Color.gray)
-                            AxisValueLabel()
-                                .foregroundStyle(Color.gray)
-                        }
-                    }
-                    .frame(width: 500,height: 200)
-                    .padding()
+        
+        func selectChart() -> [chartData]{
+            switch selectedChartIndex {
+            case 0 : return chartOneSec
+            case 1 : return chartThreeSec
+            case 2 : return chartOneMin
+            default : return []
+            }
+        }
+        
+        func startTimer() {
+            timerOneSec = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ _ in
+                if index >= fullData.count {
+                    timer?.invalidate()
+                } else {
+                    chartOneSec.append(fullData[index])
+                    index += 1
+                    t1 += 1
                 }
-                VStack(spacing:33){
-                    Text("250")
-                    Text("200")
-                    Text("150")
-                    Text("150")
-                    Text(" ")
+            }
+            timerThreeSec = Timer.scheduledTimer(withTimeInterval: 3, repeats: true){ _ in
+                if index >= fullData.count {
+                    timer?.invalidate()
+                } else {
+                    chartThreeSec.append(fullData[index])
+                    index += 3
+                    t2 += 3
                 }
-                .font(.caption)
-                .frame(width:35)
-                .foregroundColor(Color.gray)
             }
-            .background{
-//                Color.black
-            }
-        }
-        HStack(){// MARK: updatePeriod Button
-            Button {
-                updatePeriod = 1
-                indexPlus = 1
-                xDomain = 0...100
-                restartTimer()
-            } label: {
-                Text("1초")
-                    .font(.caption)
-                    .bold()
-                    .foregroundColor(Color.black)
-                    .frame(width:35, height:25)
-                    .padding(.horizontal, 1)
-                    .background{
-                        Color.blue
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
-                    }
-            } // 1초 button
-            
-            Button {
-                updatePeriod = 3
-                indexPlus = 3
-                xDomain = 0...150
-                restartTimer()
-            } label: {
-                Text("3초")
-                    .font(.caption)
-                    .bold()
-                    .foregroundColor(Color.black)
-                    .frame(width:35, height:25)
-                    .padding(.horizontal, 1)
-                    .background{
-                        Color.blue
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
-                    }
-            } // 3초 button
-            
-            Button {
-                updatePeriod = 60
-                indexPlus = 60
-                xDomain = 0...500
-                restartTimer()
-            } label: {
-                Text("1분")
-                    .font(.caption)
-                    .bold()
-                    .foregroundColor(Color.black)
-                    .frame(width:35, height:25)
-                    .padding(.horizontal, 1)
-                    .background{
-                        Color.blue
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
-                    }
-            } // 1분 button
-            Spacer()
-        }
-        .padding()
-        .onAppear {
-            restartTimer()
-        }
-    } // body
-    
-    func restartTimer() {
-        timer?.invalidate() // 기존 타이머 중지
-        timer = Timer.scheduledTimer(withTimeInterval: updatePeriod, repeats: true) { _ in
-            if index >= fullData.count {
-                timer?.invalidate()
-            } else {
-                displayData.append(fullData[index])
-                index += indexPlus
+            timerOneMin = Timer.scheduledTimer(withTimeInterval: 60, repeats: true){ _ in
+                if index >= fullData.count {
+                    timer?.invalidate()
+                } else {
+                    chartOneMin.append(fullData[index])
+                    index += 60
+                    t3 += 60
+                }
             }
         }
-    }
+        func stopTimers() {
+            timerOneSec?.invalidate()
+            timerThreeSec?.invalidate()
+            timerOneMin?.invalidate()
+        }
 }
-
-struct oneMin: View {
-
-    @Binding private var displayData: [chartData]
-    @Binding private var updatePeriod: Double
-    @Binding private var timer: Timer?
-    @Binding private var index: Int
-    @Binding private var indexPlus: Int
-    @Binding private var xDomain: ClosedRange<Double>
-    
-    var body: some View {
-        VStack (spacing : 20){
-            HStack{ // MARK: chart
-                ScrollView(.horizontal){
-                    Chart(displayData) { item in
-                        LineMark (
-                            x: .value("time", item.time),
-                            y: .value("price",item.price)
-                        )
-                    }
-                    .chartXScale(domain: xDomain)
-                    .chartYScale(domain: 50...250)
-                    .chartYAxis {
-                        AxisMarks {
-                            AxisGridLine()
-                                .foregroundStyle(Color.gray)
-                            AxisTick()
-                                .foregroundStyle(Color.gray)
-                        }
-                    }
-                    .chartXAxis {
-                        AxisMarks {
-                            AxisGridLine()
-                                .foregroundStyle(Color.gray)
-                            AxisTick()
-                                .foregroundStyle(Color.gray)
-                            AxisValueLabel()
-                                .foregroundStyle(Color.gray)
-                        }
-                    }
-                    .frame(width: 500,height: 200)
-                    .padding()
-                }
-                VStack(spacing:33){
-                    Text("250")
-                    Text("200")
-                    Text("150")
-                    Text("150")
-                    Text(" ")
-                }
-                .font(.caption)
-                .frame(width:35)
-                .foregroundColor(Color.gray)
-            }
-            .background{
-//                Color.black
-            }
-        }
-        HStack(){// MARK: updatePeriod Button
-            Button {
-                updatePeriod = 1
-                indexPlus = 1
-                xDomain = 0...100
-                restartTimer()
-            } label: {
-                Text("1초")
-                    .font(.caption)
-                    .bold()
-                    .foregroundColor(Color.black)
-                    .frame(width:35, height:25)
-                    .padding(.horizontal, 1)
-                    .background{
-                        Color.blue
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
-                    }
-            } // 1초 button
-            
-            Button {
-                updatePeriod = 3
-                indexPlus = 3
-                xDomain = 0...150
-                restartTimer()
-            } label: {
-                Text("3초")
-                    .font(.caption)
-                    .bold()
-                    .foregroundColor(Color.black)
-                    .frame(width:35, height:25)
-                    .padding(.horizontal, 1)
-                    .background{
-                        Color.blue
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
-                    }
-            } // 3초 button
-            
-            Button {
-                updatePeriod = 60
-                indexPlus = 60
-                xDomain = 0...500
-                restartTimer()
-            } label: {
-                Text("1분")
-                    .font(.caption)
-                    .bold()
-                    .foregroundColor(Color.black)
-                    .frame(width:35, height:25)
-                    .padding(.horizontal, 1)
-                    .background{
-                        Color.blue
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
-                    }
-            } // 1분 button
-            Spacer()
-        }
-        .padding()
-        .onAppear {
-            restartTimer()
-        }
-    } // body
-    
-    func restartTimer() {
-        timer?.invalidate() // 기존 타이머 중지
-        timer = Timer.scheduledTimer(withTimeInterval: updatePeriod, repeats: true) { _ in
-            if index >= fullData.count {
-                timer?.invalidate()
-            } else {
-                displayData.append(fullData[index])
-                index += indexPlus
-            }
-        }
-    }
-}
-
 
 
 #Preview {
     ContentView()
 }
+
